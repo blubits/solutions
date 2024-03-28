05ebb17802c40946c58f7c7b30f8d77db90efbbb
Maded Batara III <dev@contact.blubits.me>
2024-03-28 12:51:11 +0800

    solved: oj/10071-physics

---
+++ oj/@solved/10071-physics/physics.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+const int INF = 1e9;
+const ll LLINF = 4e18;
+const double EPS = 1e-9;
+
+int main() {
+    int v, t;
+    while (cin >> v >> t) {
+        cout << v * t * 2 << endl;
+    }
+    return 0;
+}
