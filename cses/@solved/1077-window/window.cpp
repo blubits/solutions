9019973d4ea79ec8692d3b3c41c37cad42d7104f
Maded Batara III <dev@contact.blubits.me>
2024-08-13 10:26:51 +0800

    solved: cses/1077-window

--- cses/@solved/1077-window/window.cpp
+++ cses/@solved/1077-window/window.cpp
@@ -15,32 +15,36 @@ const double EPS = 1e-9;
 
 class MedianFinder {
     multiset<int> lower, higher;
+    ll lower_sum, higher_sum;
 
     void balance() {
-        int bal = lower.size() - higher.size(); // either 1 or 0
+        int bal = lower.size() - higher.size();  // either 1 or 0
         // if bal = 2, offload from low -> high
         // if bal = -1, offload from high -> low
         if (bal < 0) {
+            int to_remove = *higher.begin();
             lower.insert(*higher.begin());
             higher.erase(higher.begin());
+            lower_sum += to_remove;
+            higher_sum -= to_remove;
         } else if (bal > 1) {
+            int to_remove = *lower.rbegin();
             higher.insert(*lower.rbegin());
             lower.erase(lower.find(*lower.rbegin()));
+            higher_sum += to_remove;
+            lower_sum -= to_remove;
         }
     }
 
-public:
-    MedianFinder() {}
+   public:
+    MedianFinder() : lower_sum(0), higher_sum(0) {}
 
-    void print() {
-        cout << "[";
-        for (auto x = lower.begin(); x != lower.end(); x++) cout << *x << ",";
-        cout << "] [";
-        for (auto x = higher.rbegin(); x != higher.rend(); x++) cout << *x << ",";
-        cout << "]" << endl;
+    ll cost() {
+        return abs(lower_sum - ll(lower.size()) * median()) +
+               abs(higher_sum - ll(higher.size()) * median());
     }
 
-    int median() {
+    ll median() {
         if (lower.empty()) return INF;
         return *lower.rbegin();
     }
@@ -50,14 +54,24 @@ public:
         // - lower = higher or lower + 1 = higher (lower < higher)
         // - lower contains <= median
         // - higher contains > median
-        if (x > median()) higher.insert(x);
-        else lower.insert(x);
+        if (x > median()) {
+            higher.insert(x);
+            higher_sum += x;
+        } else {
+            lower.insert(x);
+            lower_sum += x;
+        }
         balance();
     }
 
     void remove(int x) {
-        if (x > median()) higher.erase(higher.find(x));
-        else lower.erase(lower.find(x));
+        if (x > median()) {
+            higher.erase(higher.find(x));
+            higher_sum -= x;
+        } else {
+            lower.erase(lower.find(x));
+            lower_sum -= x;
+        }
         balance();
     }
 };
@@ -69,8 +83,11 @@ int main() {
     vector<int> A(N);
     for (int i = 0; i < N; i++) cin >> A[i];
     for (int i = 0; i < M; i++) mf.insert(A[i]);
-    cout << mf.median()
+    cout << mf.cost();
     for (int i = M; i < N; i++) {
-
+        mf.remove(A[i - M]);
+        mf.insert(A[i]);
+        cout << " " << mf.cost();
     }
+    cout << endl;
 }
